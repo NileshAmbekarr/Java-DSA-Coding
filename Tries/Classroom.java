05e1e38f61465112c5f7a9ae3bed8d4b087e16c6
Setup Github Desktop <nileshambekar9281@gmail.com>
2024-07-09 18:15:51 +0530

    TRIE

---
+++ Tries/Classroom.java
@@ -0,0 +1,47 @@
+package Tries;
+
+import java.util.Arrays;
+
+public class Classroom {
+    static class Node {
+        Node children[]= new Node[26];
+        boolean eow = false ; // end of word
+
+        Node(){
+            Arrays.fill(children, null);  // instead of using for loop , we performed the same action by using this method
+        }
+    }
+    public static Node root = new Node();
+    public static void insert(String word){  // Time Complexity = O(L) => The word length which is to be inserted \
+        Node curr = root;
+        for (int level = 0; level < word.length(); level++) {
+            int idx = word.charAt(level) - 'a';
+            if(curr.children[idx] == null)
+                curr.children[idx] = new Node();
+            curr = curr.children[idx];
+        }
+        curr.eow = true;
+    }
+    public static boolean search(String key){
+        Node curr = root;
+        for (int level = 0; level<key.length(); level++) {
+            int idx = key.charAt(level) - 'a';
+            if (curr.children[idx] == null) {
+                return false;
+            }
+            curr = curr.children[idx];
+        }
+        return curr.eow ;
+    }
+    public static void main(String[] args) {
+        String[] words = {"the", "there", "thee", "a", "any", "their", "amo"};
+        for (String word : words) {
+            insert(word);
+        }
+
+        System.out.println(search("there"));
+        System.out.println(search("amo"));
+        System.out.println(search("th"));
+
+    }
+}
