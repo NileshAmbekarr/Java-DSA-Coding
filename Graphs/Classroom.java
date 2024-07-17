cbd1cac5318b49f60fc7e9f95ae157f3a383fc8a
Setup Github Desktop <nileshambekar9281@gmail.com>
2024-07-17 23:43:58 +0530

    Implemented BFS (Breadth First Search ) Algorithm

--- Graphs/Classroom.java
+++ Graphs/Classroom.java
@@ -52,9 +52,37 @@ public class Classroom {
 
         graph[6].add(new Edge(6, 5, 1));
 
+    }
 
+    public static void bfs(ArrayList<Edge>[] graph){
+        Queue<Integer> q = new LinkedList<>();
+        boolean[] vis = new boolean[graph.length];
+        q.add(0); // starting point / vertex
+
+        while(!q.isEmpty()){
+            int curr = q.remove();
+            if(!vis[curr]){
+                System.out.print(curr + " ");
+                vis[curr] = true;
+
+                for (int i = 0; i < graph[curr].size(); i++){
+                    q.add(graph[curr].get(i).dest);
+                }
+            }
+        }
     }
     public static void main(String[] args) {
+        /*
+         *     1 ---- 3
+         *    /       | \
+         *   0        |  5 --- 6
+         *    \       | /
+         *     2 -----4
+         * */
 
+        int V = 7; // No of vertices
+        ArrayList<Edge>[] graph = new ArrayList[V];
+        createGraph(graph);
+        bfs(graph);
     }
 }
