f16437ab6d2167b700c2bdae883726862c8c926f
Setup Github Desktop <nileshambekar9281@gmail.com>
2024-08-17 15:36:01 +0530

    Practised some problems

--- Graphs/CheapestFlights.java
+++ Graphs/CheapestFlights.java
@@ -45,7 +45,7 @@ public class CheapestFlights {
         }
     }
     // Cheapest Flight function
-    public static void cheapestFlights(int[][] flights, int src, int dest, int k, int n ){
+    public static int cheapestFlights(int[][] flights, int src, int dest, int k, int n ){
         ArrayList<Edge>[] graph = new ArrayList[n];
         createGraph(flights, graph);
 
@@ -61,15 +61,31 @@ public class CheapestFlights {
                 break;
             }
 
-            for (int i = 0; i < graph[curr.vertex]; i++){
-                
+            for (int i = 0; i < graph[curr.vertex].size(); i++){
+                Edge e = graph[curr.vertex].get(i);
+                int u = e.src;
+                int v = e.dest;
+                int wt = e.wt;
+
+                //relaxation
+                if(curr.cost < dist[v] && curr.stops <= k){
+                    dist[v] = curr.cost+wt;
+                    q.add(new Info(v, dist[v], curr.stops+1));
+                }
+
             }
         }
+
+        if (dist[dest] == Integer.MAX_VALUE)
+            return -1;
+        else
+            return dist[dest];
     }
 
     public static void main(String[] args) {
         int n = 4;
         int flights[][] = {{0, 1, 100}, {1, 2, 100}, {2, 0, 100}, {1, 3, 600}, {2, 3, 200}};
         int k = 1;
+        System.out.println(cheapestFlights(flights, 0, 3, 1, n));
     }
 }
