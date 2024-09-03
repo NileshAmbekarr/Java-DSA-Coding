716d3eb412ff40bd2f83c5f38bf3709cc9f6222d
Setup Github Desktop <nileshambekar9281@gmail.com>
2024-09-03 20:58:50 +0530

    Update SubSeq_Subsets.java

--- Recursion/SubSeq_Subsets.java
+++ Recursion/SubSeq_Subsets.java
@@ -1,8 +1,20 @@
 package Recursion;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class SubSeq_Subsets {
+
+    static void ssq(int idx, int[] arr, int n, int[] nums){
+        if(idx == n){
+            System.out.println(Arrays.toString(arr));
+            return;
+        }
+        arr[idx] = nums[idx];
+        ssq(idx + 1, arr, n, nums);
+        arr[idx] = 0;
+        ssq(idx + 1, arr, n, nums);
+    }
     static ArrayList<String> getSSQ(String s){
         // Created Arraylist for storing the answer
         ArrayList<String> ans = new ArrayList<>();
@@ -22,5 +34,7 @@ public class SubSeq_Subsets {
     public static void main(String[] args) {
         ArrayList<String> ans = getSSQ("abc");
         System.out.println(ans);
+        int[] nums = {3, 1, 2};
+        ssq(0, new int[nums.length], nums.length, nums  );
     }
 }
